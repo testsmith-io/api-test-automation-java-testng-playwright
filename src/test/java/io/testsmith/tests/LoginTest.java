d83046daba884bfaeba60382a16b965fab4d0826
Roy de Kleijn <dekleijn.roy@gmail.com>
2026-08-18 23:57:17 +0200

    Version bump and fixes

--- src/test/java/io/testsmith/tests/LoginTest.java
+++ src/test/java/io/testsmith/tests/LoginTest.java
@@ -10,35 +10,42 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class LoginTest {
-    private Playwright playwright;
-    private APIRequestContext request;
+  private Playwright playwright;
+  private APIRequestContext request;
 
-    @BeforeClass
-    public void setup() {
-        playwright = Playwright.create();
-        request = playwright.request().newContext(new APIRequest.NewContextOptions()
-                .setBaseURL(System.getProperty("base.url", "https://api.practicesoftwaretesting.com")));
-    }
+  @BeforeClass
+  public void setup() {
+    playwright = Playwright.create();
+    request =
+        playwright
+            .request()
+            .newContext(
+                new APIRequest.NewContextOptions()
+                    .setBaseURL(
+                        System.getProperty("base.url", "https://api.practicesoftwaretesting.com")));
+  }
 
-    @Test
-    public void testShouldReturnAccessTokenOnSuccessfulLogin() {
-        JsonObject payload = new JsonObject();
-        payload.addProperty("email", "customer@practicesoftwaretesting.com");
-        payload.addProperty("password", "welcome01");
+  @Test
+  public void testShouldReturnAccessTokenOnSuccessfulLogin() {
+    JsonObject payload = new JsonObject();
+    payload.addProperty("email", "customer@practicesoftwaretesting.com");
+    payload.addProperty("password", "welcome01");
 
-        APIResponse response = request.post("https://api.practicesoftwaretesting.com/users/login", RequestOptions.create()
-                                                                                                                 .setData(payload));
-        Assert.assertEquals(response.status(), 200, "Unexpected status code");
+    APIResponse response =
+        request.post(
+            "https://api.practicesoftwaretesting.com/users/login",
+            RequestOptions.create().setData(payload));
+    Assert.assertEquals(response.status(), 200, "Unexpected status code");
 
-        String responseBody = response.text();
-        JsonObject responseBodyJson = JsonParser.parseString(responseBody).getAsJsonObject();
-        Assert.assertTrue(responseBodyJson.has("access_token"), "Response does not contain 'access_token'");
+    String responseBody = response.text();
+    JsonObject responseBodyJson = JsonParser.parseString(responseBody).getAsJsonObject();
+    Assert.assertTrue(
+        responseBodyJson.has("access_token"), "Response does not contain 'access_token'");
+  }
 
-    }
-
-    @AfterClass
-    public void teardown() {
-        request.dispose();
-        playwright.close();
-    }
+  @AfterClass
+  public void teardown() {
+    request.dispose();
+    playwright.close();
+  }
 }
--- src/test/java/io/testsmith/tests/RetrieveBrandsTest.java
+++ src/test/java/io/testsmith/tests/RetrieveBrandsTest.java
@@ -12,29 +12,34 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class RetrieveBrandsTest {
-    private Playwright playwright;
-    private APIRequestContext request;
+  private Playwright playwright;
+  private APIRequestContext request;
 
-    @BeforeClass
-    public void setup() {
-        playwright = Playwright.create();
-        request = playwright.request().newContext(new APIRequest.NewContextOptions()
-                .setBaseURL(System.getProperty("base.url", "https://api.practicesoftwaretesting.com")));
-    }
+  @BeforeClass
+  public void setup() {
+    playwright = Playwright.create();
+    request =
+        playwright
+            .request()
+            .newContext(
+                new APIRequest.NewContextOptions()
+                    .setBaseURL(
+                        System.getProperty("base.url", "https://api.practicesoftwaretesting.com")));
+  }
 
-    @Test
-    public void testShouldRetrieveAtLeastTwoBrands() {
-        APIResponse response = request.get("/brands");
-        Assert.assertEquals(response.status(), 200, "Unexpected status code");
+  @Test
+  public void testShouldRetrieveAtLeastTwoBrands() {
+    APIResponse response = request.get("/brands");
+    Assert.assertEquals(response.status(), 200, "Unexpected status code");
 
-        String responseBody = response.text();
-        JsonArray data = JsonParser.parseString(responseBody).getAsJsonArray();
-        Assert.assertTrue(data.size() >= 2, "Expected at least 2 brands, but got " + data.size());
-    }
+    String responseBody = response.text();
+    JsonArray data = JsonParser.parseString(responseBody).getAsJsonArray();
+    Assert.assertTrue(data.size() >= 2, "Expected at least 2 brands, but got " + data.size());
+  }
 
-    @AfterClass
-    public void teardown() {
-        request.dispose();
-        playwright.close();
-    }
+  @AfterClass
+  public void teardown() {
+    request.dispose();
+    playwright.close();
+  }
 }
